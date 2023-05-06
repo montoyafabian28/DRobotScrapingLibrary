242a4a90c5c196ebbf060a4a4f50007d92cc8969
Fabian Montoya <montoyafabian28.fmfm@gmail.com>
2023-05-06 16:41:20 -0600

    foreachbodyrow

--- src/s.js
+++ src/s.js
@@ -1,1 +1,15 @@
 console.log('s.js')
+export function forEachBodyRow (tableSelector, callback) {
+  const htmlTable = document.querySelector(tableSelector)
+  if (!htmlTable) return
+
+  const selector = 'tbody tr'
+  const innerSelector = 'td'
+  const elementsToIterate = [...htmlTable.querySelectorAll(selector)]
+    .map(tr => [...tr.querySelectorAll(innerSelector)]
+      .map(cell => cell.innerText.trim().toLocaleLowerCase()))
+
+  elementsToIterate.forEach((row, index, array) => {
+    callback(row, index, array)
+  })
+}
